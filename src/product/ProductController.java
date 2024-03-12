d753eafa3a7547a912a7b85fee8e77d14f1bfb74
viniciusac92 <vinicius.campello@hotmail.com>
2024-03-12 13:03:14 -0300

    feat(develop): adicionados atributo e metodos da ProductController.

--- src/product/ProductController.java
+++ src/product/ProductController.java
@@ -3,19 +3,37 @@ package product;
 import java.util.ArrayList;
 
 public class ProductController {
-
     private ArrayList products;
 
     public String create(ProductModel payload) throws Exception{
-        // ProductModel novoProduto = new ProductModel();
-
         if (payload.getPriceInCents() <= 0) {
             throw new Exception("O preço do produto precisa ser um número positivo.");
         }
 
-
-
         return "Produto " + payload.getName() + " adicionado.";
+    }
+
+    public void read() throws Exception{
+        // ProductModel novoProduto = new ProductModel();
+
+        // if (payload.getPriceInCents() <= 0) {
+            throw new Exception("O preço do produto precisa ser um número positivo.");
+        // }
+
+
+
+        // return "Produto " + payload.getName() + " adicionado.";
+    }
+
+    public Integer retrievePrice(String barcode) throws Exception{
+        ProductModel produto = new ProductModel();
+
+        // TODO: buscar todos os codigos 
+        if (produto.getBarCode() == barcode) {
+            throw new Exception("O preço do produto precisa ser um número positivo.");
+        }
+
+        return produto.getPriceInCents();
     }
 
 
